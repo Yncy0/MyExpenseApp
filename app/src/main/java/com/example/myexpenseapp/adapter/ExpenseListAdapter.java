593700286bc2c7f9b5c38ed951cac4fea50a6d7f
Yncy0 <paalayancy@gmail.com>
2024-04-27 08:41:52 +0800

    init viewmodel

---
+++ app/src/main/java/com/example/myexpenseapp/adapter/ExpenseListAdapter.java
@@ -0,0 +1,16 @@
+package com.example.myexpenseapp.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ExpenseListAdapter {
+
+    class ExpenseViewHolder extends RecyclerView.ViewHolder{
+
+        public ExpenseViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
